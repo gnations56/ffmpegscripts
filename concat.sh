74e1e7d7417c9b98e21a2ef1a1d50a5a0394f673
gnations56 <gnationsonyoutube@outlook.com>
2019-10-26 05:53:30 -0700

    added semicolon

--- concat.sh
+++ concat.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 cd ./to_concat
-for f in ./*.mp4 do mv "$f" "${f// /_}"; done
+for f in ./*.mp4; do mv "$f" "${f// /_}"; done
 for f in ./*.mp4; do echo "file '$f'" >> mylist.txt; done
 ffmpeg -f concat -i mylist.txt -c copy $(cat output.txt).mp4
 
