41d4fa2e09b60f0d8be1d02aae0f60878c501bda
gnations56 <gnationsonyoutube@outlook.com>
2019-10-26 17:26:24 -0700

    added conversion to aac audio

--- concat.sh
+++ concat.sh
@@ -2,5 +2,5 @@
 cd ./to_concat
 for f in ./*.mp4; do mv "$f" "${f// /_}"; done
 for f in ./*.mp4; do echo "file '$f'" >> mylist.txt; done
-ffmpeg -f concat -safe 0 -i mylist.txt -c copy $(cat output.txt).mp4
+ffmpeg -f concat -safe 0 -i mylist.txt -c:v copy -c:a aac $(cat output.txt).mp4
 
